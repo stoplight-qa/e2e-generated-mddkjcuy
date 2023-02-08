2d2832a76f060130dc6d7f0ce2e1a649ac3eb3b9
qa-bdghz-2-kjyo@example.com <qa-bdghz-2-kjyo@example.com>
2023-02-08 15:45:09 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15182.stoplight-dev.com/api/v1/projects/cHJqOjE3Nw/spectral.js?branch_slug=main&token=4365aaf3-1dbd-4362-ac05-d559afc69e17";
+export default { extends: ruleset };
